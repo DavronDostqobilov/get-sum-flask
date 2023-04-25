269639d6cf0fd058cb4740e56ebe7e4d44c80bbf
DavronDostqobilov <dostqobilovdavron885@gamil.com>
2023-04-25 19:41:37 +0500

    app

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask, request
 app = Flask(__name__)
 
 
-@app.route('/api/get-sum/', methods=['GET'])
+@app.route('/api', methods=['GET'])
 def get_sum():
     """
     this function returns sum of two number from request data.
@@ -22,8 +22,23 @@ def get_sum():
     """
     a=request.args.get('a')
     b=request.args.get('b')
-    s=a+b
+    s=int(a)+int(b)
     return {"sum":s}
+# @app.route('/')
+# def value():
+#    return "http://127.0.0.1:4080/product<br>http://127.0.0.1:4080/home<br>http://127.0.0.1:4080/contact<br>http://127.0.0.1:4080/about"
+# @app.route('/home')
+# def home():
+#    return "Home Page"
+# @app.route('/about')
+# def about():
+#    return "About Page"
+# @app.route('/contact')
+# def contact():
+#    return "Contact Page"
+# @app.route('/product')
+# def product():
+#    return "Product Page"
 
 if __name__ == "__main__":
     app.run(debug=True,port='4080')
